06d88760a2745a031ac59e6957d4a88c20131e0d
Gabriel Gonzalez <hgglez92@gmail.com>
2019-09-06 16:04:41 -0600

    adding search bar

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class App extends React.Component {
-   render() {
-      return <div>Hola Mundo</div>;
-   }
-}
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
