be099e08d9a829276d3be3b0dca72acf43aed8cc
Gabriel Gonzalez <hgglez92@gmail.com>
2019-09-06 12:41:47 -0600

    adding spinner - changing look of the main page

--- weather-mini-app/src/SeasonDisplay.jsx
+++ weather-mini-app/src/SeasonDisplay.jsx
@@ -1,7 +1,41 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
+const seasonConfig = {
+   summer: {
+      text: "Let's hit the beach",
+      iconName: "sun"
+   },
+   winter: {
+      text: "Burr, it is chilly",
+      iconName: "snowflake"
+   }
+};
+
+const getSeason = (lat, month) => {
+   if (month > 2 && month < 9) {
+      return lat > 0 ? "summer" : "winter";
+   } else {
+      return lat > 0 ? "winter" : "summer";
+   }
+};
 const SeasonDisplay = props => {
-   return <div>{props.lat}</div>;
+   const season = getSeason(props.lat, new Date().getMonth());
+   const { text, iconName } = seasonConfig[season];
+
+   return (
+      <div className={`ui container ${season}`}>
+         <div className="icon-container top">
+            <i className={`massive icon ${iconName}`} />
+         </div>
+         <div className="text-container">
+            <h1>{text}</h1>
+         </div>
+         <div className="icon-container bottom">
+            <i className={`massive icon ${iconName}`} />
+         </div>
+      </div>
+   );
 };
 
 export default SeasonDisplay;
