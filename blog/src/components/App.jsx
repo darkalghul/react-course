bbf01e448cc868675473514798c174b434672a58
Gabriel Gonzalez <hgglez92@gmail.com>
2019-09-26 11:32:38 -0600

    adding general style

--- blog/src/components/App.jsx
+++ blog/src/components/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import PostList from "./PostList";
 
 const App = () => {
-   return <div>App</div>;
+   return (
+      <div className="ui container grid">
+         <div className="ui centered row">
+            <div className="eight wide column">
+               <PostList />
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default App;
