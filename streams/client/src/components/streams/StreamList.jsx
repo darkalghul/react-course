2eb56a0e5bdd4307fd5ed4a2a535dc8f5b8e1e6f
Gabriel Gonzalez <hgglez92@gmail.com>
2019-10-10 13:03:06 -0600

    showing list of streams, changing routing, adding FontAwesome

--- streams/client/src/components/streams/StreamList.jsx
+++ streams/client/src/components/streams/StreamList.jsx
@@ -1,7 +1,87 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-   return <div>Stream List</div>;
+class StreamList extends React.Component {
+   componentDidMount() {
+      this.props.fetchStreams();
+   }
+
+   checkAdminUser = stream => {
+      if (stream.userId === this.props.currentUser) {
+         return (
+            <>
+               <span className="float-right">
+                  <span className="btn btn-danger">
+                     <i className="fas fa-trash-alt"></i>
+                  </span>
+               </span>
+               <span className="float-right mr-1">
+                  <span className="btn btn-primary">
+                     <i className="fas fa-edit"></i>
+                  </span>
+               </span>
+            </>
+         );
+      } else {
+         return <></>;
+      }
+   };
+
+   renderList() {
+      return this.props.streams.map(stream => {
+         return (
+            <li key={stream.id} className="list-group-item clearfix">
+               <div className=" float-left">
+                  <a href="#" className="d-block h5 mb-1">
+                     {stream.title}
+                  </a>
+                  <p className="mb-0 float-left">{stream.description}</p>
+               </div>
+               {this.checkAdminUser(stream)}
+            </li>
+         );
+      });
+   }
+
+   renderCreate() {
+      if (this.props.isSignedIn) {
+         return (
+            <div className="col-12 col-md-12 clearfix">
+               <div className="float-right">
+                  <Link to="/streams/new" className="btn btn-primary">
+                     Create Stream
+                  </Link>
+               </div>
+            </div>
+         );
+      }
+   }
+   render() {
+      return (
+         <div className="container my-3">
+            <div className="row">
+               <div className="col-12 col-md-12">
+                  <h2>Streams</h2>
+                  <ul className="list-group">{this.renderList()}</ul>
+               </div>
+            </div>
+            <div className="row mt-3">{this.renderCreate()}</div>
+         </div>
+      );
+   }
+}
+
+const mapStateToProps = state => {
+   return {
+      streams: Object.values(state.streams),
+      currentUser: state.auth.userId,
+      isSignedIn: state.auth.isSignedIn
+   };
 };
 
-export default StreamList;
+export default connect(
+   mapStateToProps,
+   { fetchStreams }
+)(StreamList);
